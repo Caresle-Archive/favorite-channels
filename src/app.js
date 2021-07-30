10aa6e30ca372cafe4758b2d76d247d523b453f2
Caresle <carabesle@gmail.com>
2021-07-30 16:03:08 -0600

    End my channels design

--- src/app.js
+++ src/app.js
@@ -23,6 +23,8 @@ app.use(indexRoutes)
 app.use(channelsRoutes)
 app.use(userRoutes)
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.listen(PORT, () => {
 	console.log(`Server on PORT ${PORT}`)
 })
