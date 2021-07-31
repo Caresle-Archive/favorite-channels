2d63db57b94397cdd9b73a10f21d0c87f50c2388
Caresle <carabesle@gmail.com>
2021-07-30 19:13:43 -0600

    Add render db data on channels

--- src/controllers/channels.controllers.js
+++ src/controllers/channels.controllers.js
@@ -1,15 +1,14 @@
 const Channel = require('../models/channel')
 
-const renderChannels = (req, res) => {
-	res.render('channels/index')
+const renderChannels = async (req, res) => {
+	const channels = await Channel.find().lean()
+	console.log(channels)
+	res.render('channels/index', {
+		channels: channels
+	})
 }
 
 const newChannel = async (req, res) => {
-	// const newChannelInfo = new Channel({
-	// 	name: req.body.channel_name,
-	// 	url: req.body.channel_url
-	// })
-	// newChannelInfo.save().then(() => res.redirect('/channels'))
 	const newChannelInfo = {
 		name: req.body.channel_name,
 		url: req.body.channel_url 
